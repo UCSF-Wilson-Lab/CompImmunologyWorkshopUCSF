fed179b10ec5b3c485ffba116eada0386d687ca3
RaviDandekar <ravi.dandekar@gmail.com>
2024-04-17 12:38:00 -0700

    Updated immcantation commands to accomodate v4.5.0 which was run locally

--- 0_vdj_alignments/2_run_workshop_immcantation.bash
+++ 0_vdj_alignments/2_run_workshop_immcantation.bash
@@ -1,17 +1,17 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # Goal: Generate results for 10X MS BCR & TCR data
 #  - there are two BCR and TCR samples
-#  - Immcantation v4.3.0 supports the analysis of these data (v4.5.0 had issues with BLASTP)
+#  - Immcantation v4.5.0 supports the analysis of these data
 
 # Immcantation Documentation:
 # https://immcantation.readthedocs.io/en/stable/
 
 # Install container on your computer:
-# docker pull immcantation/suite:4.3.0
+# docker pull immcantation/suite:4.5.0
 
 # 1. Make sure data exists within container (use 'exit' command to exit container):
-# docker run --name WORKSHOP -it immcantation/suite:4.3.0 bash 
+# docker run --name Workshop -it immcantation/suite:4.5.0 bash 
 
 # Arguments
 # - Input formatted sample name
@@ -32,17 +32,3 @@ changeo-10x -s $READS_BCR -a $ANNOTATIONS_BCR -x 0.15 -n BCR_CSFPB -o $OUT_DIR_B
 
 # >>> TCR
 changeo-10x -s $READS_TCR -a $ANNOTATIONS_TCR -x 0.00 -n TCR_CSFPB -o $OUT_DIR_TCR -p $NPROC -t tr -m $MODEL -f airr
-
-# >>> process TCR Alpha chain results
-
-
-cd $OUT_DIR_TCR
-DefineClones.py -d TCR_CSFPB_light_productive-T.tsv --act first --model $MODEL --dist 0.00
-CreateGermlines.py -d TCR_CSFPB_light_productive-T_clone-pass.tsv -o TCR_CSFPB_light_germ-pass.tsv -g dmask --cloned -r /usr/local/share/germlines/imgt/human/vdj/imgt_human_TRA*
-
-# >>> process BCR light chain results
-cd $OUT_DIR_BCR
-DefineClones.py -d BCR_CSFPB_light_productive-T.tsv --act first --model $MODEL --dist 0.15
-CreateGermlines.py -d BCR_CSFPB_light_productive-T_clone-pass.tsv -o BCR_CSFPB_light_germ-pass.tsv -g dmask --cloned -r /usr/local/share/germlines/imgt/human/vdj/imgt_human_IG*
-
-
